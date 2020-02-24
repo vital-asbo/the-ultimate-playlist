2bd4bdef2f9d7e6cbd9449251d335a8bc877ad64
Vitalii Kovalenko <vitalii.kovalenko@yahoo.com>
2020-02-24 08:43:58 +0100

    Commit 3: all methods in SongController now work. Connection to local Postgres DB

--- src/main/java/com/theultimateplaylist/demo/songs/SongController.java
+++ src/main/java/com/theultimateplaylist/demo/songs/SongController.java
@@ -1,5 +1,6 @@
 package com.theultimateplaylist.demo.songs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,17 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Controller
 public class SongController {
 
-    List<Song> songList = new ArrayList<>();
-
-//    @Autowired
-//    private SongRepository songRepository;
+    @Autowired
+    private SongRepository songList;
 
     @RequestMapping("/")
     public String indexGet() {
@@ -32,22 +28,22 @@ public class SongController {
 //        songList.add(new Song(2, "Limp Bizkit", "Rollin'",
 //                "Chocolate Starfish and the Hot Dog Flavored Water", 2000, SongRating.LOVE));
 //        songList.add(new Song(3, "Format:B", "Chunky", "Chunky", 2015, SongRating.LIKE));
-        songList.add(new Song(1, "Linkin Park", "Numb", "Meteora", 2003));
-        songList.add(new Song(2, "Limp Bizkit", "Rollin'",
+        songList.save(new Song(1, "Linkin Park", "Numb", "Meteora", 2003));
+        songList.save(new Song(2, "Limp Bizkit", "Rollin'",
                 "Chocolate Starfish and the Hot Dog Flavored Water", 2000));
-        songList.add(new Song(3, "Format:B", "Chunky", "Chunky", 2015));
+        songList.save(new Song(3, "Format:B", "Chunky", "Chunky", 2015));
         return "song/add-default-playlist";
     }
 
     @RequestMapping("/view-songs")
     public ModelAndView viewSongs(Model model) {
-        return new ModelAndView("song/view-songs", "songList", songList);
+        return new ModelAndView("song/view-songs", "songList", songList.findAll());
     }
 
     @RequestMapping("/remove-song")
     public ModelAndView removeSong(@RequestParam(value = "song_id") String stringId) {
         int intId = Integer.parseInt(stringId);
-        songList.remove(getSongById(intId));
+        songList.deleteById(intId);
         return new ModelAndView("redirect:/view-songs");
     }
 
@@ -58,33 +54,35 @@ public class SongController {
 
     @RequestMapping(value = "/save-song")
     public ModelAndView saveSong(@ModelAttribute(value = "song") Song song, BindingResult result) {
-        int index = songList.size();
+        int index = Integer.parseInt(String.valueOf(songList.count()));
         if (song.getId() == 0) {
             System.out.println("Adding a New Song");
             song.setId(index + 1);
-            songList.add(song);
+            songList.save(song);
         } else {
-            Song songTemp = getSongById(song.getId());
+            Song songTemp = songList.getOne(song.getId());
             songTemp.setArtist(song.getArtist());
             songTemp.setTitle(song.getTitle());
             songTemp.setAlbum(song.getAlbum());
             songTemp.setYear(song.getYear());
 //            songTemp.setSongRating(SongRating.NONE); //default value set to NONE - improve later
-         //   songList.add(songTemp);
+            songList.save(songTemp);
         }
         return new ModelAndView("redirect:/view-songs");
     }
 
     @RequestMapping(value = "/edit-song")
     public ModelAndView editSong(@RequestParam(value = "song_id") String song_id) {
-        Song song = getSongById(Integer.parseInt(song_id));
+        int idParsed = Integer.parseInt(song_id);
+        Song song = songList.getOne(idParsed);
         return new ModelAndView("song/add-song", "song", song);
     }
+//
+//
+    //    private Song getSongById(@RequestParam int id) {
+    //        return songList.stream().filter(f -> f.getId() == id).findFirst().orElse(null);
 
-
-    private Song getSongById(@RequestParam int id) {
-        return songList.stream().filter(f -> f.getId() == id).findFirst().orElse(null);
-    }
+    //    }
 
 
 }
